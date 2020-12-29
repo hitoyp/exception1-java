7c4672ad0af14ffcfbcea4c3c7ecc93c78853a9c
hitoyp <74926675+hitoyp@users.noreply.github.com>
2020-12-29 20:12:41 -0300

    Add files via upload

---
+++ curso-java/src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservetion;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Room number: ");
+		int number = sc.nextInt();
+		System.out.println("Check-in date (dd/mm/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.println("Check-Out date (dd/mm/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		if (!checkOut.after(checkIn)) {
+			System.out.println("error!! IN RESERVATION: CHECK-OUT DATE MUST BE AFTER CHECK-IN DATE ");
+		}else {
+			Reservetion reservetion = new Reservetion(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservetion);
+			
+			System.out.println();
+			System.out.println("ENTER DATE UPTADE THE RESERVATION: ");
+			System.out.println("Check-in date (dd/mm/yyyy): ");
+			 checkIn = sdf.parse(sc.next());
+			System.out.println("Check-Out date (dd/mm/yyyy): ");
+			 checkOut = sdf.parse(sc.next());
+			 Date now = new Date();
+			 
+			 if(checkIn.before(now) || checkOut.before(now)) {
+				 System.out.println("error in reservation: Reservation date for uptade must be future dates: ");
+			 }else if (!checkOut.after(checkIn)) {
+				 System.out.println("error!! IN RESERVATION: CHECK-OUT DATE MUST BE AFTER CHECK-IN DATE ");
+				 
+			 }else {
+				    reservetion.updateDates(checkIn, checkOut);
+					System.out.println("reservation: " + reservetion);
+					
+			 }
+			 
+			
+		}
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
